8a78cf9ad1934086dc0c9ec50c189eb3a11f04c1
winfreywekesa148-bit <winfreywekesa148@gmail.com>
2026-02-08 19:32:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,5 +14,52 @@ PlaceVisit.prototype.addPlace = function(place) {
         console.log(`[LOG] ${place.name} enrolled with ID: ${place.id}`);
 };
 
-function Place(name, landmark, year, notes) {}
+function Place(name, landmark, year, notes) {
+    this.name = name;
+    this.landmark = landmark;
+    this.year = year;
+    this.notes = notes;
+    this.id = null;
+};
+
+const myPlaceVisited = new PlaceVisit();
+
+const gridDisplay = document.getElementById(`place-display`);
+const totalRate = document.getElementById(`total-rate`);
+
+function handleSearch() {
+    const namIn = document.getElementById(`name`);
+    const landIn = document.getElementById(`landmark`);
+    const yrIn = document.getElementById(`year`)
+    const notIn = document.getElementById(`notes`)
+    
+if (namIn.value.trim() === "") {
+            alert("Please enter name.");
+            return;
+        };
+
+const newPlace = new Place(
+    namIn.value,
+    landIn.value,
+    yrIn.value,
+    notIn.value,
+);
+
+myPlaceVisited.addPlace(newPlace);
+
+renderPlace();
+
+namIn.value = "";
+};
+
+function renderPlace() {
+        gridDisplay.innerHTML = "";
+
+const placeList = Object.values(myPlaceVisited.places);
+
+
+
+}
+
+
 
